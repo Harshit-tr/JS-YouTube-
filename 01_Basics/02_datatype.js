691f4ed25bcd441d4848c52d63a08bb7be42ec81
Harshit-tr <tripathiharshitkumar2@gmail.com>
2025-05-19 01:19:02 +0000

    datatypes

--- 01_Basics/02_datatype.js
+++ 01_Basics/02_datatype.js
@@ -1,3 +1,20 @@
 "use strict"; //treat all Js code as newer version
 
 // alert("hello") // we are using nodejs, not browser
+
+let name = "harshit"
+let age = 20
+let isLoggedIn = false
+
+//Premitive datatypes
+// numbers => 2 to power 53
+// bigint => For bigger numbers
+//string => ""
+// boolean => true or false
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+//Object
+
+console.log(typeof "harshit")
