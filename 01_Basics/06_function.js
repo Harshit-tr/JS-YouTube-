0fa2d33aee9507efe93b36609903366c87c9b15d
Harshit-tr <tripathiharshitkumar2@gmail.com>
2025-05-19 13:51:07 +0000

    memory

--- 01_Basics/06_function.js
+++ 01_Basics/06_function.js
@@ -4,4 +4,22 @@ const myFunction = function(){
 myFunction()
 console.log(typeof myFunction)
 
-//Stack memomry(Primitive) , Heap(Non-Primitive)
+//Stack memomry(Primitive) , Heap memory (Non-Primitive)
+
+let myName = "harshittripathi"
+
+let anothername = myName
+anothername = "Harsh"
+console.log(anothername)
+console.log(myName)
+
+let user1 = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let user2 = user1
+
+user2.email = "harshit@gmail.com"
+console.log(user1.email)
+console.log(user2.email)
