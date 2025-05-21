5a4039f3265c74ea2958056d7946190d46db8e84
Harshit-tr <tripathiharshitkumar2@gmail.com>
2025-05-21 07:03:21 +0000

    scope

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -9,3 +9,30 @@ console.log("Inner loop : ", a)
 console.log(a)
 // console.log(b)
 // console.log(c)
+
+
+function one(){
+    const username = "Harshit"
+
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+    // console.log(website)
+
+    two()
+}
+
+// one()
+
+function addone(num){
+    return num + 1
+}
+console.log(addone(5))
+
+
+
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(5))
