7fe5bdba489740f0b356d9bcdf39a247b48e6549
Harshit-tr <tripathiharshitkumar2@gmail.com>
2025-05-23 07:56:07 +0000

    for of loop , map

---
+++ 05_itrration/three.js
@@ -0,0 +1,28 @@
+//for of
+
+// const arr = [1,2,3,4,5]
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// const greeting = "Hello world!"
+
+// for (const greet of greeting) {
+//     console.log(`Each char is ${greet}`)
+// }
+
+
+//Maps
+
+const map = new Map()
+
+map.set('IN' , "India")
+map.set('USA' , "United state of America")
+
+console.log(map)
+
+
+for (const [key , value] of map){
+    console.log(key , ':-' , value)
+}
