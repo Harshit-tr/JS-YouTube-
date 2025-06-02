8bd7e416db807a9f56650f6d2ae322a57c2e81ba
Harshit-tr <tripathiharshitkumar2@gmail.com>
2025-06-02 07:14:11 +0000

    color swither

---
+++ 07_projects/Project1/color.js
@@ -0,0 +1,26 @@
+const buttons = document.querySelectorAll('.button')
+
+const body = document.querySelector("body")
+
+// buttons.forEach( function(button) {
+//    button.addEventListener('click', function(e){
+//    if(e.target.id === 'grey'){
+//     body.style.backgroundColor = e.target.id
+//    }
+//     if(e.target.id === 'blue'){
+//     body.style.backgroundColor = e.target.id
+//    }
+//    if(e.target.id === 'black'){
+//     body.style.backgroundColor = e.target.id
+//    }
+//    if(e.target.id === 'red'){
+//     body.style.backgroundColor = e.target.id
+//    }
+//    })
+// })
+
+buttons.forEach(button => {
+   button.addEventListener('click', (e)=>{
+      body.style.backgroundColor = e.target.id
+   })
+})
